97aa4c3691dc8c5b55434021e8503d957ad86aae
Adil <adildhar@gmail.com>
2013-05-06 11:53:02 +0530

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,15 @@
+<?php
+// Read the config and set up the database connection
+$config = require 'config/local.config.php';
+$dbconfig = $config['db_master'];
+$pdo = new PDO('mysql:host=' . $dbconfig['host'] . ';dbname=' . $dbconfig['dbname'], $dbconfig['user'], $dbconfig['pass']);
+
+// Make sure we have access to the database for this application
+$result = $pdo->query('Select * from user where user_name='$username');
+
+if (1 === $result->rowCount()) {
+    echo '<div style="color:darkgreen; font-size: xx-large;">OK!</div> I can see the application database.';
+} else {
+    echo '<div style="color:red; font-size: xx-large;">Whoops!</div> I can\'t see the application database.';
+}
+?>
