6291fac4e2368f7edb728cdfafb9ec8d531ce1b2
Adil <adildhar@gmail.com>
2013-05-06 11:33:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
  <html>
  <body>
-<h1>Hello this is Demo Employee Application! ------ Running on LAMP</h1>!<br><br>
-<form action="welcome.php" method="post">!<br>
- Name: <input type="text" name="username">!<br>
- Password: <input type="password" name="password">!<br><br>
- <input type="submit" name="Login">
+<h1>Hello this is Demo Employee Application! ------ Running on LAMP</h1><br><br>
+<form action="welcome.php" method="post"><br>
+ Name: <input type="text" name="username"><br>
+ Password: <input type="password" name="password"><br><br>
+ <input type="submit" label="Login">
  </form>
  </body>
  </html> 
