0883f4fc222f0ab67569466b70b26e3acf5222d2
Adil <adildhar@gmail.com>
2013-05-06 17:01:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ p {color:blue;}
 </head>
  <body>
 <h1 align="center">Hello this is Demo Employee Database Application! ------ Running on LAMP</h1><br><br>
-<p><form action="welcome.php" method="post" onsubmit="return validateForm()"><br>
+<p><form action="welcome.php" method="post"><br>
 <table border='1' align="center"><tr><td>Name: </td><td><input type="text" name="username" required></td></tr>
  <tr><td>Password: </td><td><input type="password" name="password" required></td></tr>
  <tr><td colspan = "2" align="right"> <input type="submit" value="Login"></td></tr></table>
