acdf88b1c45dd8ef7f0e995a547335f3e9990f4a
Adil <adildhar@gmail.com>
2013-05-06 14:58:09 +0530

    Update create-table.php

--- create-table.php
+++ create-table.php
@@ -1,4 +1,21 @@
-<?php
-echo "<form action='createrecord.php' method='post'><table border='1' align='center'><tr><td>Employee Name: </td><td><input type='text' name='emp_name'></td></tr><tr><td>Employee Address: </td><td><input type='text' name='emp_address'></td></tr><tr><td>Employee Email: </td><td><input type='text' name='emp_email'</td></tr><tr><td>Employee Salary: </td><td><input type='text' name='emp_salary'</td>/tr><tr><td colspan = "2" align="right"> <input type="submit" value="Login"></td></tr></table></form>";
+<!DOCTYPE html>
+ <html>
+<head>
+<style type="text/css">
+h1 {color:red;}
+p {color:blue;}
+</style>
+</head>
+ <body>
+<h1 align="center">Create Record in Employee Database Application! ------ Running on LAMP</h1><br><br>
+<p><form action="createrecord.php" method="post"><br>
+<table border='1' align='center'>
+<tr><td>Employee Name: </td><td><input type='text' name='emp_name'></td></tr>
+<tr><td>Employee Address: </td><td><input type='text' name='emp_address'></td></tr>
+<tr><td>Employee Email: </td><td><input type='text' name='emp_email'</td></tr>
+<tr><td>Employee Salary: </td><td><input type='text' name='emp_salary'</td>/tr>
+<tr><td colspan = "2" align="right"> <input type="submit" value="Login"></td></tr></table></form>";
+</form></p>
+ </body>
+ </html> 
 
-?>
