cbad276f0abb86946127ba3499085591bf89509f
Adil <adildhar@gmail.com>
2013-05-06 14:17:38 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -18,7 +18,7 @@ if ($uvalue = "$username")
 echo "<h1> Welcome to Employee Records Database</h1>";
 echo "<href link='showemployees.php'>show Employee Records</href>";
 echo "<href link='createrecord.php'>Create Employee Record</href>";
-
+}
 else
 {
 echo "Login Incorrect! Please try again";
