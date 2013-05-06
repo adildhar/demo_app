1b6d1cb9eac81939f7d4b07e6c19088453bf81d7
Adil <adildhar@gmail.com>
2013-05-06 17:00:47 +0530

    Update empform.php

--- empform.php
+++ empform.php
@@ -10,10 +10,10 @@ p {color:blue;}
 <h1 align="center">Create Record in Employee Database Application! ------ Running on LAMP</h1><br><br>
 <p><form action="newentry.php" method="post"><br>
 <table border='1' align='center'>
-<tr><td>Employee Name: </td><td><input type='text' name='emp_name'></td></tr>
-<tr><td>Employee Address: </td><td><input type='text' name='emp_address'></td></tr>
-<tr><td>Employee Email: </td><td><input type='text' name='emp_email'</td></tr>
-<tr><td>Employee Salary: </td><td><input type='text' name='emp_salary'</td></tr>
+<tr><td>Employee Name: </td><td><input type='text' name='emp_name' required></td></tr>
+<tr><td>Employee Address: </td><td><input type='text' name='emp_address' required></td></tr>
+<tr><td>Employee Email: </td><td><input type='text' name='emp_email' required</td></tr>
+<tr><td>Employee Salary: </td><td><input type='text' name='emp_salary' required</td></tr>
 <tr><td colspan = "2" align="right"> <input type="submit" value="Create Employee"></td></tr></table>
 </form></p>
 <br><br><a href='showemployees.php'><h3>Show Employees</h3></a>
