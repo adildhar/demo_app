f501bebc82ccc24d6f5d48733b6f9c3aa413f259
Adil <adildhar@gmail.com>
2013-05-06 14:33:37 +0530

    Update create-table.php

--- create-table.php
+++ create-table.php
@@ -5,26 +5,20 @@ echo '<h1>Running a little test...</h1>';
 $config = require 'config/local.config.php';
 $dbconfig = $config['db_master'];
 $hostname = $dbconfig['host'];
-$dbname = $dbconfig['dbname'];
+$db = $dbconfig['dbname'];
 $user = $dbconfig['user'];
 $pass = $dbconfig['pass'];
-$conn = mysql_connect("$hostname","$user",$pass);
-if(! $conn )
-{
-  die('Could not connect: ' . mysql_error());
-  echo '<div style="color:red; font-size: xx-large;">Whoops!</div> I can\'t see the application database';
-}
-print ("Connected successfully\n");
-echo '<div style="color:darkgreen; font-size: xx-large;">OK!</div>';
-print ("I can see the application database.\n");
-$sql ='CREATE TABLE employee(emp_id INT NOT NULL AUTO_INCREMENT,emp_name VARCHAR(20) NOT NULL, emp_address  VARCHAR(20) NOT NULL,emp_salary INT NOT NULL, primary key ( emp_id ))';
-mysql_select_db($dbname);
-$retval = mysql_query( $sql, $conn );
-if(! $retval )
-{
-  die('Could not create table: ' . mysql_error());
-}
-echo "Table employee created successfully\n";
-mysql_close($conn);
+$conn = mysql_connect("$hostname","$user","$pass","$db");
+
+
+
+
+
+
+
+
+
+
+
 ?>
 
