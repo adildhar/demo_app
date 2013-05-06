e8d13a7400d413914900ad17e25a1fe2b3f2d3a0
Adil <adildhar@gmail.com>
2013-05-06 14:50:46 +0530

    Update create-table.php

--- create-table.php
+++ create-table.php
@@ -16,11 +16,7 @@ if (mysqli_connect_errno())
    }
 else
 {
-echo "<form action='createrecord.php' method='post'><table border='1' align='center'><tr><td>Employee Name: </td><td><input type='text' name='emp_name'></td></tr>";
-echo "<tr><td>Employee Address: </td><td><input type='text' name='emp_address'></td></tr>";
-echo "<tr><td>Employee Email: </td><td><input type='text' name='emp_email'</td></tr>";
-echo "<tr><td>Employee Salary: </td><td><input type='text' name='emp_salary'</td>/tr>";
-echo "<tr><td colspan = "2" align="right"> <input type="submit" value="Login"></td></tr></table></form>";
+echo "<form action='createrecord.php' method='post'><table border='1' align='center'><tr><td>Employee Name: </td><td><input type='text' name='emp_name'></td></tr><tr><td>Employee Address: </td><td><input type='text' name='emp_address'></td></tr><tr><td>Employee Email: </td><td><input type='text' name='emp_email'</td></tr><tr><td>Employee Salary: </td><td><input type='text' name='emp_salary'</td>/tr><tr><td colspan = "2" align="right"> <input type="submit" value="Login"></td></tr></table></form>";
 }
 ?>
 
