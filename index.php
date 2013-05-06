366fa983b51e213da93f45df00cfb24a2efe9180
Adil <adildhar@gmail.com>
2013-05-06 13:04:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
  <html>
+<head>
+<style type="text/css">
+h1 {color:red;}
+p {color:blue;}
+</style>
+</head>
  <body>
-<h1 align="center">Hello this is Demo Employee Application! ------ Running on LAMP</h1><br><br>
-<form action="welcome.php" method="post"><br>
+<h1 align="center">Hello this is Demo Employee Database Application! ------ Running on LAMP</h1><br><br>
+<p><form action="welcome.php" method="post"><br>
 <table border='1' align="center"><tr><td>Name: </td><td><input type="text" name="username"></td></tr>
  <tr><td>Password: </td><td><input type="password" name="password"></td></tr>
  <tr><td colspan = "2" align="right"> <input type="submit" value="Login"></td></tr></table>
- </form>
+ </form></p>
  </body>
  </html> 
