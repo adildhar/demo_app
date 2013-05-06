7c31d920dba34ffaccbea0e8edc9f98f93a44796
Adil <adildhar@gmail.com>
2013-05-06 15:05:13 +0530

    Update createrecord.php

--- createrecord.php
+++ createrecord.php
@@ -1,1 +1,28 @@
-dd
+
+<?php
+$emp_name = $_POST['emp_name'];
+$emp_address = $_POST['emp_address'];
+$emp_email = $_POST['emp_email'];
+$emp_salary = $_POST['emp_salary'];
+$config = require 'config/local.config.php';
+$dbconfig = $config['db_master'];
+$hostname = $dbconfig['host'];
+$db = $dbconfig['dbname'];
+$user = $dbconfig['user'];
+$pass = $dbconfig['pass'];
+$con=mysqli_connect("$hostname","$user","$pass","$db");
+if (mysqli_connect_errno())
+   {
+   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   echo "Login Incorrect! Please try again";
+   echo "<br><br><a href='index.php'>Go Back</a>";
+   }
+else
+{
+
+
+
+
+}
+?>
+
